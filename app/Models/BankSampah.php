7a7ddd1850db651d096460293a0a856c063a9e21
denifelixe <deni.victoria@gmail.com>
2023-07-30 19:27:47 +0700

    add Soft Delete

--- app/Models/BankSampah.php
+++ app/Models/BankSampah.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BankSampah extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
 
     protected $table = 'bank_sampah';
 
